3ef8ab68383d05bd16f9c609af956f0bc5a7e572
Hajk <tunczorx@gmail.com>
2018-04-23 13:41:24 +0200

    Learning how to use enum in simple task.

--- LearningEnumerationTypesAndCollectionsV0.2/Form1.cs
+++ LearningEnumerationTypesAndCollectionsV0.2/Form1.cs
@@ -14,7 +14,15 @@ namespace LearningEnumerationTypesAndCollectionsV0._2
     {
         public Form1()
         {
-            InitializeComponent();
+            InitializeComponent();           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            Card card = new Card((Suits)random.Next(4), (Values)random.Next(1,14)); //So we gonna take 1 of 4 possible types of cards, and then we gonna take random value of card for example "Five of Hearts"
+            string cardName = card.Name;
+            MessageBox.Show(cardName);
         }
     }
 }
